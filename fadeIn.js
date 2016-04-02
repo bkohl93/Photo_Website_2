8834893cdd66de7390d9af1a692b2c3e2eeda412
Ben Kohl <bkohl93@gmail.com>
2016-04-02 13:07:03 -0400

    changed fadeIn.js to use loops

--- fadeIn.js
+++ fadeIn.js
@@ -5,41 +5,20 @@ function showHome()
 
 function showWork()
 {
-	$("#pic1").load(function () { $(this).fadeIn(300); });
-	$("#pic2").load(function () { $(this).fadeIn(300); });
-	$("#pic3").load(function () { $(this).fadeIn(300); });
-	$("#pic4").load(function () { $(this).fadeIn(300); });
-	$("#pic5").load(function () { $(this).fadeIn(300); });
-	$("#pic6").load(function () { $(this).fadeIn(300); });
-	$("#pic7").load(function () { $(this).fadeIn(300); });
-	$("#pic8").load(function () { $(this).fadeIn(300); });
-	$("#pic9").load(function () { $(this).fadeIn(300); });
-	$("#pic10").load(function () { $(this).fadeIn(300); });
-	$("#pic11").load(function () { $(this).fadeIn(300); });
-	$("#pic12").load(function () { $(this).fadeIn(300); });
-	$("#pic13").load(function () { $(this).fadeIn(300); });
-	$("#pic14").load(function () { $(this).fadeIn(300); });
-	$("#pic15").load(function () { $(this).fadeIn(300); });
-	$("#pic16").load(function () { $(this).fadeIn(300); });
-	$("#pic17").load(function () { $(this).fadeIn(300); });
-	$("#pic18").load(function () { $(this).fadeIn(300); });
-	$("#pic19").load(function () { $(this).fadeIn(300); });
-	$("#pic20").load(function () { $(this).fadeIn(300); });
-	$("#pic21").load(function () { $(this).fadeIn(300); });
-	$("#pic22").load(function () { $(this).fadeIn(300); });
+	for (i = 0; i <= 22; i++)
+	{
+		var img = $("#pic"+i);
+		img.load(function () { $(this).fadeIn(300); });
+	}
 }
 
 function showShows()
 {
-	$("#img1").load(function () { $(this).fadeIn(300); });
-	$("#img2").load(function () { $(this).fadeIn(300); });
-	$("#img3").load(function () { $(this).fadeIn(300); });
-	$("#img4").load(function () { $(this).fadeIn(300); });
-	$("#img5").load(function () { $(this).fadeIn(300); });
-	$("#img6").load(function () { $(this).fadeIn(300); });
-	$("#img7").load(function () { $(this).fadeIn(300); });
-	$("#img8").load(function () { $(this).fadeIn(300); });
-	$("#img9").load(function () { $(this).fadeIn(300); });
+	for (i = 0; i <= 9; i++)
+	{
+		var img = $("#img"+i);
+		img.load(function () { $(this).fadeIn(300); });
+	}
 }
 
 function showContact()
